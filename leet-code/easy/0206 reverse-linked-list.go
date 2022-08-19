09363cb5e90c4836405ff720bfbd665de755d1ed
Eudes Lemos <eudesllemos@gmail.com>
2022-08-18 23:28:56 -0300

    Solve leetcode problems

---
+++ leet-code/easy/0206 reverse-linked-list.go
@@ -0,0 +1,21 @@
+package leet
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	cur := head
+	var prev *ListNode
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+
+		cur = next
+	}
+
+	return prev
+}
