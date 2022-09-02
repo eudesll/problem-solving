e25b9f2f4be43246d2186025e78bdfd4acfcf907
Eudes Lemos <eudesllemos@gmail.com>
2022-09-02 13:03:32 -0300

    Improve 0347 solution from leetcode

--- leet-code/medium/0347 top-k-frequent-elements.go
+++ leet-code/medium/0347 top-k-frequent-elements.go
@@ -1,23 +1,49 @@
 package leet
 
-import "sort"
-
 func topKFrequent(nums []int, k int) []int {
 	counter := map[int]int{}
 	for _, n := range nums {
 		counter[n]++
 	}
 
-	rank := make([]int, len(counter))
-	i := 0
-	for n := range counter {
-		rank[i] = n
-		i++
+	// O(KlogN)
+	allFrequencies := make([][]int, len(nums))
+	for n, f := range counter {
+		nf := allFrequencies[f-1]
+		if nf == nil {
+			nf = []int{}
+		}
+
+		allFrequencies[f-1] = append(nf, n)
 	}
 
-	sort.Slice(rank, func(i, j int) bool {
-		return counter[rank[i]] > counter[rank[j]]
-	})
+	rank := []int{}
+	for i := len(nums) - 1; k > 0; i-- {
+		nf := allFrequencies[i]
+		if nf == nil {
+			continue
+		}
 
-	return rank[0:k]
+		smallest := len(nf)
+		if k < smallest {
+			smallest = k
+		}
+
+		rank = append(rank, nf[0:smallest]...)
+		k -= smallest
+	}
+
+	// O(NlogN)
+	// rank := make([]int, len(counter))
+	// i := 0
+	// for n := range counter {
+	// 	rank[i] = n
+	// 	i++
+	// }
+	// sort.Slice(rank, func(i, j int) bool {
+	// 	return counter[rank[i]] > counter[rank[j]]
+	// })
+	// rank = rank[0:k]
+
+	return rank
 }
