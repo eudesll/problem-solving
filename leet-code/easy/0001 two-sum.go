fafad8994775c5b694214645fbd1808d30b85c25
Eudes Lemos <eudes@calopsita.local>
2024-02-22 18:43:11 -0300

    Redoing old leetcode problems

---
+++ leet-code/easy/0001 two-sum.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+	numsM := map[int]int{}
+	out := make([]int, 2)
+
+	for i, num := range nums {
+			if ix, ok := numsM[target-num]; ok {
+					return []int{ix, i}
+			}
+
+			numsM[num] = i
+	}
+
+	return out
+}
