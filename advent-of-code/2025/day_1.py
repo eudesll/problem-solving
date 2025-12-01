3f69cb0b9bf0e7af48173c33e95c237b1feb8200
Eudes Lemos <eudesllemos@gmail.com>
2025-12-01 13:10:20 -0300

    Solve day 1 of advent of code 2025

---
+++ advent-of-code/2025/day_1.py
@@ -0,0 +1,34 @@
+def stop_on_zero(data): # 1
+  dial, r = 50, 0
+  for d in data:
+    dial += (int(d[1:]) if d[0] == "R" else -int(d[1:]))
+    dial %= 100
+
+    r += 0 if dial else 1
+  
+  return r
+
+def pass_through_zero(data): # 2
+  dial, r = 50, 0
+  for d in data:
+    val = int(d[1:]) if d[0] == "R" else -int(d[1:])
+    dial += val
+
+    if val > 0:
+        r += dial // 100 - (dial - val) // 100
+    elif val < 0:
+        r += (dial - val - 1) // 100 - (dial - 1) // 100
+  
+  return r
+
+def __main__():
+  filenames = ["test", "input"]
+  for filename in filenames:
+    file = open("day_1_" + filename, "r")
+    data = file.read().split("\n")
+
+    print(filename)
+    print(f"- 1:", stop_on_zero(data))
+    print(f"- 2:", pass_through_zero(data))
+
+__main__()
